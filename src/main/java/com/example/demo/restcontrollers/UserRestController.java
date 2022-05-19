2ec346b05254f1cde83ce199dd26013ac45ef5cb
Alfonso José Barroso Barajas <alfonsobarrosobarajas@gmail.com>
2022-05-19 13:38:09 -0600

    feat(USERS): Agrega restcontroller

---
+++ src/main/java/com/example/demo/restcontrollers/UserRestController.java
@@ -0,0 +1,28 @@
+package com.example.demo.restcontrollers;
+
+import com.example.demo.entities.UserEntity;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Ejemplo de RestController
+ * Se agrega el servicio UserService :)
+ */
+@RestController
+@RequestMapping("/users")
+public class UserRestController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping()
+    public List<UserEntity> find() {
+        return userService.find();
+    }
+
+}
